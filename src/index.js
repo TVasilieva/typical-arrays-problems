e3df223d18e9196fea3eab70699330dc6fa45dcd
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2020-10-03 22:19:08 +0300

    feat: add code min/max/avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) { 
+    if (Array.isArray(array) && array.length > 0) {
+        return Math.min.apply(null,array);
+    } else {
   return 0;
+}
 }
 
 exports.max = function max (array) {
+    if (Array.isArray(array) && array.length > 0) {
+        return Math.max.apply(null,array);
+    } else {
   return 0;
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (Array.isArray(array) && array.length > 0) {
+            let result = 0;
+            for (let i = 0; i < array.length; i++) {
+              result += array[i];
+            }
+            return result/(array.length);
+        } else {
+            return 0;
+        }
 }
